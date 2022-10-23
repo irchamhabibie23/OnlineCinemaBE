163983329f1c2104f5388ee16c330a76c71c8205
ircham <MUHAMMAD.HABIBIE2@btpn.com>
2022-10-23 20:59:51 +0700

    push

---
+++ src/controllers/ipCounter.js
@@ -0,0 +1,25 @@
+const countIp = {};
+
+exports.ipCounter = async (req, res) => {
+  try {
+    const{socket:{remoteAddress}} = req
+    const IP = remoteAddress.split(':')[3]
+    if (!countIp[IP]) {
+      countIp[IP] = 1
+    }
+    else {
+      countIp[IP] = countIp[IP] + 1
+    }
+    if (countIp[IP] === 3) {
+      console.log("Anjar")
+    }
+    res.send({data: countIp})
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      status: "failed",
+      message: "server error",
+      pesankesalahan: res,
+    })
+  }
+}
